e8f7eb0dc7f804e68754a484ee0c61f287c77d3e
Luis Mendez <luis@codekunst.com>
2020-03-23 10:15:27 +0100

    Improved/refactored JavaScript getChartData

--- FlowtiAppPerformance.js
+++ FlowtiAppPerformance.js
@@ -1,69 +1,48 @@
+window.onload = function() {
+    if(document.getElementById('avgexectimechart')){
+        var ctx = document.getElementById('avgtime').getContext('2d');
+        window.avgtime = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                datasets: [{
+                    label: false,
+                    backgroundColor: '#6e1970',
+                    data: getAvgChartData(ctx.canvas.id)
+                }]
+            },
+            options: barOptions,
+        });
+    }
+    if(document.getElementById('avgmemorychart')){
+        var ctx = document.getElementById('avgmemory').getContext('2d');
+        window.avgmemory = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                datasets: [{
+                    label: false,
+                    backgroundColor: '#329e96',
+                    data: getAvgChartData(ctx.canvas.id)
+                }]
+            },
+            options: barOptions,
+        });
+    }
+};
+
 var barOptions = {
-    responsive: true,            
-    legend: { display: false} ,
+    responsive: true,
+    legend: { display: false},
     title: { display: false }
 };
-window.onload = function() {
-    if(document.getElementById('db-btn')){
-        var ctx = document.getElementById('avgexectime').getContext('2d');
-        window.barExecTime = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                datasets: [{
-                    label: '',
-                    barPercentage: 0.5,
-                    barThickness: 6,
-                    maxBarThickness: 8,
-                    minBarLength: 0,
-                    backgroundColor: '#6e1970',
-                    data: []
-                }]
-            },
-            options: barOptions,
+function getAvgChartData(chart){
+    $.getJSON('./getAvgChartData?data='+chart, function(res){
+        chart = window[chart];
+        $(res).each(function(k,v) {
+            chart.data.labels.push(v.label);
+            chart.data.datasets.forEach((dataset) => {
+                dataset.data.push(v.val);
+            });
         });
-    
-        var ctx = document.getElementById('avgmemory').getContext('2d');
-        window.barMemory = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                datasets: [{
-                    label: '',
-                    barPercentage: 0.5,
-                    barThickness: 6,
-                    maxBarThickness: 8,
-                    minBarLength: 0,
-                    backgroundColor: '#329e96',
-                    data: []
-                }]
-            },
-            options: barOptions,
-        });
-    }
-};
-
-$(document).ready(function(){
-    if(document.getElementById('db-btn')){
-        $.getJSON('./getChartDataExecTime', function(data){addDataExecTime(data)});
-        $.getJSON('./getChartDataMemory', function(data){addDataMemory(data)});
-    }
-})
-
-function addDataExecTime(data) {
-    $(data).each(function(k,v) {
-        window.barExecTime.data.labels.push(v.label);
-        window.barExecTime.data.datasets.forEach((dataset) => {
-            dataset.data.push(v.val);
-        });
+        chart.update();
     });
-    window.barExecTime.update();
 }
-function addDataMemory(data) {
-    $(data).each(function(k,v) {
-        window.barMemory.data.labels.push(v.label);
-        window.barMemory.data.datasets.forEach((dataset) => {
-            dataset.data.push(v.val);
-        });
-    });
-    window.barMemory.update();
-}
-
